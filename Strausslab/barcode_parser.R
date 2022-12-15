e4a61bdd3b4490e5f44a80d662ee660c8bec73e4
Emma Dawson <69990652+ekd90262@users.noreply.github.com>
2022-12-15 16:23:57 -0500

    Update barcode_parser.R

--- Strausslab/barcode_parser.R
+++ Strausslab/barcode_parser.R
@@ -19,7 +19,7 @@ df <- do.call("cbind", split(my_data, rep(c(1, 2), length.out = nrow(my_data))))
 colnames(df)
 df <- janitor::clean_names(df)
 
-df<- cSplit(df, 1, sep = "-", type.convert = FALSE)
+df<- cSplit(df, 1, sep = "-", type.convert = FALSE) #https://stackoverflow.com/questions/31999002/split-string-in-each-column-for-several-columns
 df <- cSplit(df, 8, sep = ".", type.convert = FALSE)
 df <- cSplit(df, 1, sep = ":", type.convert = FALSE)
 
